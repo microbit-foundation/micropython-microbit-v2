microbit-foundation/micropython-microbit-v2

commit c41d43933aaea23e7921d9a24b04a40a3a674514

codal_app: Update pin list to match changes in codal-microbit.

src/codal_app/microbithal.h
@@ -26,27 +26,28 @@
 #ifndef MICROPY_INCLUDED_CODAL_APP_MICROBITHAL_H
 #define MICROPY_INCLUDED_CODAL_APP_MICROBITHAL_H
 
-#define MICROBIT_HAL_PIN_P0     (2)
-#define MICROBIT_HAL_PIN_P1     (3)
-#define MICROBIT_HAL_PIN_P2     (4)
-#define MICROBIT_HAL_PIN_P3     (31)
-#define MICROBIT_HAL_PIN_P4     (28)
-#define MICROBIT_HAL_PIN_P5     (14)
-#define MICROBIT_HAL_PIN_P6     (37)
-#define MICROBIT_HAL_PIN_P7     (11)
-#define MICROBIT_HAL_PIN_P8     (10)
+// These numbers refer to indices in the (private) pin_obj table.
+#define MICROBIT_HAL_PIN_P0     (0)
+#define MICROBIT_HAL_PIN_P1     (1)
+#define MICROBIT_HAL_PIN_P2     (2)
+#define MICROBIT_HAL_PIN_P3     (3)
+#define MICROBIT_HAL_PIN_P4     (4)
+#define MICROBIT_HAL_PIN_P5     (5)
+#define MICROBIT_HAL_PIN_P6     (6)
+#define MICROBIT_HAL_PIN_P7     (7)
+#define MICROBIT_HAL_PIN_P8     (8)
 #define MICROBIT_HAL_PIN_P9     (9)
-#define MICROBIT_HAL_PIN_P10    (30)
-#define MICROBIT_HAL_PIN_P11    (23)
+#define MICROBIT_HAL_PIN_P10    (10)
+#define MICROBIT_HAL_PIN_P11    (12)
 #define MICROBIT_HAL_PIN_P12    (12)
-#define MICROBIT_HAL_PIN_P13    (17)
-#define MICROBIT_HAL_PIN_P14    (1)
-#define MICROBIT_HAL_PIN_P15    (13)
-#define MICROBIT_HAL_PIN_P16    (34)
-#define MICROBIT_HAL_PIN_P19    (26)
-#define MICROBIT_HAL_PIN_P20    (32)
-#define MICROBIT_HAL_PIN_USB_TX (45)
-#define MICROBIT_HAL_PIN_USB_RX (46)
+#define MICROBIT_HAL_PIN_P13    (13)
+#define MICROBIT_HAL_PIN_P14    (14)
+#define MICROBIT_HAL_PIN_P15    (15)
+#define MICROBIT_HAL_PIN_P16    (16)
+#define MICROBIT_HAL_PIN_P19    (17)
+#define MICROBIT_HAL_PIN_P20    (18)
+#define MICROBIT_HAL_PIN_USB_TX (30)
+#define MICROBIT_HAL_PIN_USB_RX (31)
 
 // These match the micro:bit v1 constants.
 #define MICROBIT_HAL_PIN_PULL_UP (0)

src/codal_app/microbithal.cpp
@@ -32,56 +32,39 @@ extern "C" {
 #include "microbithal.h"
 
 static NRF52Pin *const pin_obj[] = {
-    &uBit.io.speaker,
-    &uBit.io.P14,
     &uBit.io.P0,
     &uBit.io.P1,
     &uBit.io.P2,
-    &uBit.io.microphone,
-    &uBit.io.uart_rx,
-    &uBit.io.unused1,
-    &uBit.io.scl,
-    &uBit.io.P9,
-    &uBit.io.P8,
-    &uBit.io.P7,
-    &uBit.io.P12,
-    &uBit.io.P15,
+    &uBit.io.P3,
+    &uBit.io.P4,
     &uBit.io.P5,
-    &uBit.io.row3,
-    &uBit.io.sda,
+    &uBit.io.P6,
+    &uBit.io.P7,
+    &uBit.io.P8,
+    &uBit.io.P9,
+    &uBit.io.P10,
+    &uBit.io.P11,
+    &uBit.io.P12,
     &uBit.io.P13,
-    &uBit.io.unused2,
-    &uBit.io.row5,
+    &uBit.io.P14,
+    &uBit.io.P15,
+    &uBit.io.P16,
+    &uBit.io.P19,
+    &uBit.io.P20,
+    &uBit.io.face,
+    &uBit.io.speaker,
     &uBit.io.runmic,
+    &uBit.io.microphone,
+    &uBit.io.sda,
+    &uBit.io.scl,
     &uBit.io.row1,
     &uBit.io.row2,
-    &uBit.io.P11,
+    &uBit.io.row3,
     &uBit.io.row4,
-    &uBit.io.irq1,
-    &uBit.io.P19,
-    &uBit.io.unused3,
-    &uBit.io.P4,
-    &uBit.io.unused4,
-    &uBit.io.P10,
-    &uBit.io.P3,
-
-    &uBit.io.P20,
-    &uBit.io.unused5,
-    &uBit.io.P16,
-    &uBit.io.unused6,
-    &uBit.io.face,
-    &uBit.io.P6,
-
-    &uBit.io.col1,
-    &uBit.io.col2,
-    &uBit.io.col3,
-    &uBit.io.col4,
-    &uBit.io.col5,
-    &uBit.io.buttonA,
-    &uBit.io.buttonB,
-
+    &uBit.io.row5,
     &uBit.io.usbTx,
     &uBit.io.usbRx,
+    &uBit.io.irq1,
 };
 
 static Button *const button_obj[] = {

commit 4727557246334dfc2b142a316a7706d540c02b48

codal_port: Fix pin_speaker HAL number.
Fixes a regression introduced by c41d43933aaea23e7921d9a24b04a40a3a674514

src/codal_app/microbithal.h
@@ -46,6 +46,7 @@
 #define MICROBIT_HAL_PIN_P16    (16)
 #define MICROBIT_HAL_PIN_P19    (17)
 #define MICROBIT_HAL_PIN_P20    (18)
+#define MICROBIT_HAL_PIN_SPEAKER (20)
 #define MICROBIT_HAL_PIN_USB_TX (30)
 #define MICROBIT_HAL_PIN_USB_RX (31)
 

src/codal_port/microbit_pin.c
@@ -48,7 +48,7 @@ const microbit_pin_obj_t microbit_p16_obj = {{&microbit_dig_pin_type},  16, MICR
 const microbit_pin_obj_t microbit_p19_obj = {{&microbit_dig_pin_type},  19, MICROBIT_HAL_PIN_P19, MODE_I2C};
 const microbit_pin_obj_t microbit_p20_obj = {{&microbit_dig_pin_type},  20, MICROBIT_HAL_PIN_P20, MODE_I2C};
 
-const microbit_pin_obj_t microbit_pin_speaker_obj = {{&microbit_dig_pin_type}, 31, 0, MODE_UNUSED};
+const microbit_pin_obj_t microbit_pin_speaker_obj = {{&microbit_dig_pin_type}, 31, MICROBIT_HAL_PIN_SPEAKER, MODE_UNUSED};
 
 static mp_obj_t microbit_pin_get_mode_func(mp_obj_t self_in) {
     microbit_pin_obj_t *self = (microbit_pin_obj_t*)self_in;