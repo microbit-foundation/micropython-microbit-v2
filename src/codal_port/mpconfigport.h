06d36cb7eaf52a04751a32202ad3fb24920d72e7
Damien George <damien@micropython.org>
2020-10-01 23:06:48 +1000

    codal_port: Enable input() builtin function.

--- src/codal_port/mpconfigport.h
+++ src/codal_port/mpconfigport.h
@@ -58,6 +58,7 @@
 
 // Fine control over Python builtins, classes, modules, etc
 #define MICROPY_PY_BUILTINS_MEMORYVIEW          (1)
+#define MICROPY_PY_BUILTINS_INPUT               (1)
 #define MICROPY_PY_BUILTINS_HELP                (1)
 #define MICROPY_PY_BUILTINS_HELP_TEXT           microbit_help_text
 #define MICROPY_PY_BUILTINS_HELP_MODULES        (1)
