cf08a1d6be157698385d8ac35bf3e9bd30562a71
Damien George <damien@micropython.org>
2023-11-14 12:23:14 +1100

    codal_port/modaudio: Extend AudioFrame constructor to take opt size.
    Signed-off-by: Damien George <damien@micropython.org>

--- src/codal_port/modaudio.h
+++ src/codal_port/modaudio.h
@@ -29,14 +29,12 @@
 
 #include "py/runtime.h"
 
-#define LOG_AUDIO_CHUNK_SIZE (5)
-#define AUDIO_CHUNK_SIZE (1 << LOG_AUDIO_CHUNK_SIZE)
-
 #define SOUND_EXPR_TOTAL_LENGTH (72)
 
 typedef struct _microbit_audio_frame_obj_t {
     mp_obj_base_t base;
-    uint8_t data[AUDIO_CHUNK_SIZE];
+    size_t size;
+    uint8_t data[];
 } microbit_audio_frame_obj_t;
 
 extern const mp_obj_type_t microbit_audio_frame_type;
@@ -45,7 +43,6 @@ extern const mp_obj_module_t audio_module;
 void microbit_audio_play_source(mp_obj_t src, mp_obj_t pin_select, bool wait, uint32_t sample_rate);
 void microbit_audio_stop(void);
 bool microbit_audio_is_playing(void);
-microbit_audio_frame_obj_t *microbit_audio_frame_make_new(void);
 
 const char *microbit_soundeffect_get_sound_expr_data(mp_obj_t self_in);
 
